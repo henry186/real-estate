94af5fa2cf2ef8ac835313c266e89be362673f3f
Unknown <unknown@example.com>
2020-06-09 02:46:11 +0800

    plot 3 maps
    max,min,and mean of the real-estate price per square meters.
    (exclude Peng Hu, a part of data of it is na)
    (the min map doesn't have reference value because almost all of the data are 0)

--- Taiwan-estate.r
+++ Taiwan-estate.r
@@ -1,3 +1,34 @@
+library(rgeos)                      
+library(rgdal)               #install.package("rgdal",type = "source",repos = NULL)
+library(maptools)
+library(ggplot2)
+library(plyr)
+library(RColorBrewer)
+library(gridExtra)
+
+setwd("~/downloads/real-estate")
+
+#read shp file
+taiwanmap<-readShapeSpatial("twmapdata/TOWN_MOI_1090324.shp")               #readshpfile
+taiwanmap.county<-fortify(taiwanmap,region = "COUNTYID")                    #transform shpdf to df
+taiwanmap.df<-fortify(taiwanmap,region = "TOWNID")  #transform shpdfto df
+
+#remove the Spratly Islands and Pratas Island
+x<-which(taiwanmap.county$lat<=20)
+taiwanmap.county<-taiwanmap.county[-x,]
+x<-which(taiwanmap.df$lat<=20)
+taiwanmap.df<-taiwanmap.df[-x,]
+
+#transform the encoding
+taiwanmap$TOWNNAME<- iconv(taiwanmap$TOWNNAME,from = "UTF-8", to ="CP950")  
+taiwanmap$COUNTYNAME<- iconv(taiwanmap$COUNTYNAME,from = "UTF-8", to ="CP950")  
+
+#create a df include townname & TownID
+NameIdTable <- data.frame(id = taiwanmap$TOWNID,name = taiwanmap$TOWNNAME)  
+NameIdTable<-cbind(NameIdTable,
+                   idlabel=substr(NameIdTable$id,start = 1,stop= 1))
+
+
 setwd("104-4")
 
 ReadFile<-function(i)
@@ -27,37 +58,71 @@ ReadAllFile <- function(i,year)
   cat("file1",year,"-",i,"\n")
   return(adply(1:26,.margins = 1,.fun = ReadFile))
 }
-taiwany<-adply(1:4,.margins = 1,.fun = ReadAllFile,year=103)
 
 #read estate data and clean up the data
-taiwan<-adply(1:26,.margins = 1,.fun = ReadFile)
+taiwan<-adply(1:4,.margins = 1,.fun = ReadAllFile,year=103)
 taiwan <-taiwan[c(30,2,23,24,3,9,16,10,11,12,13,14,17,18,19,20,21,22,25,26,27)]
 colnames(taiwan)[c(1,2,4)]<-c("id","name","PricePerSqrtm")
 taiwan$PricePerSqrtm<-as.numeric(taiwan$PricePerSqrtm)
 
 #calculate the avg of price/squaremeters
-countyplot<-ddply(taiwan,.(id),summarise,y = round(mean(PricePerSqrtm,na.rm = T))/10000)
+priceMean<-ddply(taiwan,.(id),summarize,mean = round(mean(PricePerSqrtm,na.rm = T)))
 
-#1090607 on myPC                               
+#calculate the max value of price/square meters
+priceMax<-ddply(taiwan,.(id),summarise,max=max(PricePerSqrtm,na.rm = T))
+
+#calculate the min value of price/square meters
+priceMin<-ddply(taiwan,.(id),summarise,min=min(PricePerSqrtm,na.rm = T))
+
+#merge the map data with estate data                              
 p<-data.frame(name=taiwanmap$COUNTYNAME,                                                 
               id=taiwanmap$COUNTYID,
               stringsAsFactors = F)
 
-p<-merge(p,countyplot,by= "id")
-finalplot2<-merge(taiwanmap.county,p,by="id",all.p=T)
-
-twcmap<-ggplot()+geom_polygon(data = finalplot2,
-                              aes(x=long,
-                                  y=lat,
-                                  group=group,
-                                  fill=y),
-                              color="black",
-                              size=0.25)+
+mean_plot<-merge(p,priceMean,by= "id")
+meanPlot<-merge(taiwanmap.county,mean_plot,by="id",all.mean_plot=T)
+priceMeanMap<-ggplot()+geom_polygon(data = meanPlot,
+                                    aes(x=long,
+                                        y=lat,
+                                        group=group,
+                                        fill=mean),
+                                    color="black",
+                                    size=0.25)+
   scale_fill_gradientn(
     colours = brewer.pal(9,"Reds"))+
   theme_void()+
   coord_map()+
-  labs(title = "price of the real estate")
+  labs(title = "mean price of the real estate")
 
-      #tablea<-NameIdTable[NameIdTable$idlabel==LETTERS[1],]
-      #a<-merge(taiwana,tablea,by = "name")
+max_plot<-merge(p,priceMax,by= "id")
+maxPlot<-merge(taiwanmap.county,max_plot,by="id",all.max_plot=T)
+priceMaxMap<-ggplot()+geom_polygon(data = maxPlot,
+                                    aes(x=long,
+                                        y=lat,
+                                        group=group,
+                                        fill=max),
+                                    color="black",
+                                    size=0.25)+
+  scale_fill_gradientn(
+    colours = brewer.pal(9,"Reds"))+
+  theme_void()+
+  coord_map()+
+  labs(title = "max price of the real estate")
+##要把0弄掉
+min_plot<-merge(p,priceMin,by= "id")
+minPlot<-merge(taiwanmap.county,min_plot,by="id",all.min_plot=T)
+priceMinMap<-ggplot()+geom_polygon(data = minPlot,
+                                   aes(x=long,
+                                       y=lat,
+                                       group=group,
+                                       fill=min),
+                                   color="black",
+                                   size=0.25)+
+  scale_fill_gradientn(
+    colours = brewer.pal(9,"Reds"))+
+  theme_void()+
+  coord_map()+
+  labs(title = "min price of the real estate")
+
+#plot the map of the max,min,mean of the  estate price
+#grid.arrange(priceMaxMap,priceMinMap,priceMeanMap,nrow = 2)
