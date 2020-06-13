0b5513ece4e4619402fddef2c81a5ea12946898c
Unknown <acs105131@gm.ntcu.edu.tw>
2020-06-13 12:23:27 +0800

    fix error
    read csv file set stringAsFactor = F.

--- Taiwan-estate.r
+++ Taiwan-estate.r
@@ -5,19 +5,22 @@ library(ggplot2)
 library(plyr)
 library(RColorBrewer)
 library(gridExtra)
+library(dplyr)
 
-setwd("~/downloads/real-estate")
+setwd("~/cmder/real-estate/real-estate")
 
-#read shp file
-taiwanmap<-readShapeSpatial("twmapdata/TOWN_MOI_1090324.shp")               #readshpfile
-taiwanmap.county<-fortify(taiwanmap,region = "COUNTYID")                    #transform shpdf to df
-taiwanmap.df<-fortify(taiwanmap,region = "TOWNID")  #transform shpdfto df
-
+#read shp file and use the county region
+taiwanmap<-readShapeSpatial("twmapdata/TOWN_MOI_1090324.shp")        #readshpfile
+taiwanmap.county<-fortify(taiwanmap,region = "COUNTYID")             #transform shpdf to df
 #remove the Spratly Islands and Pratas Island
 x<-which(taiwanmap.county$lat<=20)
 taiwanmap.county<-taiwanmap.county[-x,]
-x<-which(taiwanmap.df$lat<=20)
-taiwanmap.df<-taiwanmap.df[-x,]
+
+
+#taiwanmap.df<-fortify(taiwanmap,region = "TOWNID")  #transform shpdfto df
+#remove the Spratly Islands and Pratas Island
+#x<-which(taiwanmap.df$lat<=20)
+#taiwanmap.df<-taiwanmap.df[-x,]
 
 #transform the encoding
 taiwanmap$TOWNNAME<- iconv(taiwanmap$TOWNNAME,from = "UTF-8", to ="CP950")  
@@ -27,6 +30,7 @@ taiwanmap$COUNTYNAME<- iconv(taiwanmap$COUNTYNAME,from = "UTF-8", to ="CP950")
 NameIdTable <- data.frame(id = taiwanmap$TOWNID,name = taiwanmap$TOWNNAME)  
 NameIdTable<-cbind(NameIdTable,
                    idlabel=substr(NameIdTable$id,start = 1,stop= 1))
+
 
 
 setwd("104-4")
@@ -40,10 +44,11 @@ ReadFile<-function(i)
     x<-read.table(filename,header = T,sep=",",
                   quote = "",
                   row.names = NULL,
-                  fill= T,fileEncoding="UTF-8-BOM")
+                  fill= T,fileEncoding="UTF-8-BOM",
+                  stringsAsFactors = F)
     x<-x[-1,]
     cat(sprintf("read file %s\n",filename))
-    return(cbind(x,ID=rep(LETTERS[i],nrow(x))))
+    return(cbind(x,ID=LETTERS[i]))
   }
   
   else{
@@ -60,19 +65,23 @@ ReadAllFile <- function(i,year)
 }
 
 #read estate data and clean up the data
-taiwan<-adply(1:4,.margins = 1,.fun = ReadAllFile,year=103)
-taiwan<-taiwan%>%filter(交易標的!="土地"&交易標的!="車位"&單價元平方公尺>0)
+taiwan<-adply(1,.margins = 1,.fun = ReadAllFile,year=103)
+taiwan<-taiwan%>%filter(交易標的!="土地"&交易標的!="車位")
 taiwan <-taiwan[c(30,2,23,24,3,9,16,10,11,12,13,14,17,18,19,20,21,22,25,26,27)]
-taiwan[3]<- as.numeric(taiwan[[3]])
-taiwan[6]<- as.numeric(taiwan[[6]])+19110000
-taiwan[7]<- as.numeric(taiwan[[7]])
+
 colnames(taiwan)[c(1,2,4)]<-c("id","name","PricePerSqrtm")
-taiwan$PricePerSqrtm<-as.numeric(taiwan$PricePerSqrtm)
+
+taiwan[3]<- as.numeric(taiwan[[3]])
+taiwan[4]<- as.numeric((taiwan[[4]]))
+taiwan[6]<- as.numeric(taiwan[[6]])+19110000
+taiwan[7]<- as.numeric(taiwan[[7]])+19110000
+taiwan<-taiwan%>%filter(交易標的!="土地"&交易標的!="車位")
 
 #transfirm to class date
 a<-taiwan$交易年月日
 taiwan$交易年月日<-paste(substr(a,1,4),substr(a,5,6),substr(a,7,8),sep="-")
 taiwan[6]<-as.Date(taiwan[[6]])
+
 #calculate the avg of price/squaremeters
 priceMean<-ddply(taiwan,.(id),summarize,mean = round(mean(PricePerSqrtm,na.rm = T)))
 
